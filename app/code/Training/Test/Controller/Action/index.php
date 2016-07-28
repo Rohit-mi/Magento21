9100187d8f50307e1ee5351fd1b3fd05542f6fb8
Rohit <rohit@marutiinfosoft.com>
2016-07-28 11:42:05 +0530

    unt 2 question 7,8

--- app/code/Training/Test/Controller/Action/index.php
+++ app/code/Training/Test/Controller/Action/index.php
@@ -7,7 +7,18 @@
 namespace Training\Test\Controller\Action;
 class Index extends \Magento\Framework\App\Action\Action
 {
+    /**
+     *Test action index
+     */
     public function execute() {
         $this->getResponse()->appendBody("HELLO WORLD");
+    }
+    /**
+     * Check if admin has permission to visit related pages
+     * @return bool
+    */
+    protected function _isAllowed(){
+        $secret = $this->getRequest()->getParam('secret');
+        return isset($secret) && (int)$secret==1;
     }
 }
