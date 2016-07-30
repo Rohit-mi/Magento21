f3f84282543ea53dbc02154421c3533fa82259e8
Rohit <rohit@marutiinfosoft.com>
2016-07-31 02:19:27 +0530

    unt 3 que 6 create custom template and assign in block

--- app/code/Training/Test/Block/Product/View/Description.php
+++ app/code/Training/Test/Block/Product/View/Description.php
@@ -4,6 +4,6 @@ namespace Training\Test\Block\Product\View;
 class Description extends \Magento\Framework\View\Element\Template
 {
   public function beforeToHtml(\Magento\Catalog\Block\Product\View\Description $originalBlock) {
-        $originalBlock->getProduct()->setDescription('test description');
+        $originalBlock->setTemplate('Training_Test::description.phtml')
         }
 }
